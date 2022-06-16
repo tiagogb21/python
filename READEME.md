3595ed4148d79be56642b9039d897754c33e429b
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-16 16:44:56 -0300

    adicionando read me

---
+++ READEME.md
@@ -0,0 +1,7 @@
+Esse é um projeto que pede para o usuário escolher um número.
+
+Caso o número seja menor, retorna o número é menor e é mostrada a imagem de um cachorro.
+
+Caso o número seja maior, retorna o número é maior e é mostrada a imagem de um cachorro.
+
+Caso o usuário acerte, ele recebe os parabéns!
--- python.py
+++ python.py
@@ -11,8 +11,16 @@ def hello():
 
 
 @app.route('/user/<username>')
+# para poder usar o parâmetro da url, precisamos passar um parâmetro para a função
 def show_user_profile(username):
     return 'Welcome, %s' % escape(username)
+
+
+# <converter:variable_name>
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
 
 
 if __name__ == "__main__":
